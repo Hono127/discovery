52dbe922f006d6014885d340caa79580958be6ca
hiro <hiro@hironoMacBook-Air.local>
2023-09-12 22:58:37 +0900

    feat:addTodo_function

--- next-todo-app/src/app/pages/api.ts
+++ next-todo-app/src/app/pages/api.ts
@@ -1,8 +1,19 @@
-import { Todo } from "./types";
+import { Task } from "./types";
 
 // apiのfetchは必ずaysnc関数忘れずに
-export const getAllTodos = async (): Promise<Todo[]> => {
+export const getAllTodos = async (): Promise<Task[]> => {
   const res = await fetch(`http://localhost:3001/todos`, { cache: "no-store" }); // SSR
   const todos = res.json();
   return todos;
+};
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/todos`, {
+    method: "POST", //追加(POST)作業のためmethod: "POST"
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodos = res.json();
+  return newTodos;
 };
