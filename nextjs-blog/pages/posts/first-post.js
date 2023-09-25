49b979042ec6b6a3ffcc941c9addc80ac0b85cd1
hiro <>
2023-09-25 22:50:06 +0900

    nextjsチュート

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        ></Script>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  );
+}
