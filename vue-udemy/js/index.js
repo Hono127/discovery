f7252f623ff5e40b8a4bc04bef3036dd72462e9d
hiro <>
2023-10-01 11:29:37 +0900

    セクション7:監視プロパティ（ウォッチャ） 61~63

--- vue-udemy/js/index.js
+++ vue-udemy/js/index.js
@@ -1,16 +1,39 @@
 const app = Vue.createApp({
   data: () => ({
+    // 簡易app用
     newItem: "",
     todos: [],
-    message: "Hello vue.js",
+    // 監視プロパティ用
+    message: "Hello Vue.js",
+    km: 0,
+    m: 0,
+    colors: [{ name: "Red" }, { name: "Green" }, { name: "Yellow" }],
+    // 税金文用
     basePrice: "",
   }),
+  watch: {
+    // 監視プロパティ用
+    km: function (value) {
+      console.log(value);
+      this.km = value;
+      this.m = value * 1000;
+    },
+    colors: {
+      handler: function (newValue, oldValue) {
+        console.log("変更完了");
+      },
+      // ネストされたオブジェクトも監視するには下記を追記
+      deep: true,
+    },
+  },
   methods: {
+    onClick: function () {
+      this.colors[1].name = "White";
+    },
+    // todo用
     addItem: function (event) {
-      // console.log("クリック");
       if (this.newItem == "") return;
       let todo = {
-        // this=data
         item: this.newItem,
         isDone: false,
       };
@@ -25,6 +48,7 @@ const app = Vue.createApp({
     reverseMessage: function () {
       return this.message.split("").reverse().join();
     },
+    // 税金分用
     includeTaxPrice: {
       get: function () {
         return this.basePrice * 1.1;
