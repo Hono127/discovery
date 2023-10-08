59f13dfc4d66646a7b2c347978cc922bde5d3bcd
hiro <>
2023-10-09 08:55:37 +0900

    nuxt

---
+++ nuxt-environment/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <header>ヘッダー</header>
+    <slot />
+    <footer>&copy; hiroki</footer>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+header {
+  background-color: aqua;
+}
+footer {
+  background-color: aqua;
+}
+.container {
+  font-size: 24px;
+  border: solid 1px orange;
+}
+</style>
---
+++ nuxt-environment/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Top Page</h1>
+    <p>{{ users }}</p>
+  </div>
+</template>
+
+<script setup>
+const {data:users} = await useFetch('https://jsonplaceholder.typicode.com/users')
+</script>
