e783e7432a3c5125f98fd031f4815ffa8d03f7ac
hiro <>
2023-10-20 00:13:39 +0900

    test

--- blog/src/app/api/blog/route.ts
+++ blog/src/app/api/blog/route.ts
@@ -1,41 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-// prisma/clientのインスタンス化
 const prisma = new PrismaClient();
 
-// 取得する前にDBに接続する処理
 export async function main() {
   try {
-    // prisma.$connect関数は`npm i @prisma/client`のインストールが必要
     await prisma.$connect;
   } catch (err) {
-    return Error("DBに接続失敗しました");
+    return Error("faild to connect");
   }
 }
 
-// ブログ全記事取得
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
-    // postというmodelから全記事取得（post.findMany()）
+
     const posts = await prisma.post.findMany();
-    return NextResponse.json({ message: "成功しました" }, { status: 200 });
+    return NextResponse.json({ message: "success" }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({ message: "失敗しました" }, { status: 500 });
+    return NextResponse.json({ message: "faild" }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
 };
-// ブログ投稿用
+
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
-    // postというmodelから全記事取得（post.findMany()）
+
     const posts = await prisma.post.findMany();
-    return NextResponse.json({ message: "成功しました" }, { status: 200 });
+    return NextResponse.json({ message: "success" }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({ message: "失敗しました" }, { status: 500 });
+    return NextResponse.json({ message: "faild" }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">Hello</main>;
+  return (
+    <div className="w-full flex flex-col justify-center text-center h-screen bg-black font-banana text-white shadow-2xl gap-56"></div>
+  );
 }
